daa03ada0898494719dd3c30f32536949f1ed428
MiamiKnight13 <knightofmiami@gmail.com>
2026-01-06 23:42:35 +0300

    Last Update

--- SlayTheBot/SlayTheBot/Program.cs
+++ SlayTheBot/SlayTheBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
@@ -8,12 +10,10 @@ namespace SlayTheBot
 {
     internal class Program
     {
-        public const string TOKEN = "8186413878:AAFfvEBoHzLEwTq5B5ahVO6glVgJmdUi-q8";
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Host bot = new Host(TOKEN);
+            Host bot = new Host(Environment.GetEnvironmentVariable("TG_BOT_QB_TOKEN"));
             bot.Start();
             Console.ReadLine();
         }
@@ -21,6 +21,7 @@ namespace SlayTheBot
 
     class Host
     {
+        public const string OwnerId = "1369750317";
         TelegramBotClient bot;
         public Dictionary<long?, UserState> _states = new Dictionary<long?, UserState>();
         public List<Tournament> _tournaments = new List<Tournament>();
@@ -30,6 +31,26 @@ namespace SlayTheBot
         public int tPrice;
         public int tId;
 
+        public string UsersFilePath = "tournaments.json";
+
+        private void LoadTournaments()
+        {
+            if (File.Exists(UsersFilePath))
+            {
+                var json = File.ReadAllText(UsersFilePath);
+                var loaded = JsonSerializer.Deserialize<List<Tournament>>(json);
+                _tournaments = loaded ?? new List<Tournament>();
+                Console.WriteLine("tournaments have been loaded!");
+            }
+        }
+
+        private void SaveTournaments()
+        {
+            var json = JsonSerializer.Serialize(_tournaments, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(UsersFilePath, json);
+            Console.WriteLine("tournaments are saved!");
+        }
+
         public Host(string token)
         {
             bot = new TelegramBotClient(token);
@@ -37,13 +58,14 @@ namespace SlayTheBot
 
         public void Start()
         {
+            LoadTournaments();
             bot.StartReceiving(UpdateHandler, ExcrptionHandler);
             Console.WriteLine("The bot has been started!");
             Console.ReadLine();
         }
 
         private async Task ExcrptionHandler(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
-        {
+        { 
             Console.WriteLine(exception.Message);
         }
 
@@ -103,12 +125,43 @@ namespace SlayTheBot
                     await bot.SendMessage(chatId, "send the user id you want to add");
                     state.isWaitingForIdToAdd = true;
                 }
-                else if(text != null && state.isWaitingForIdToAdd)
+                else if(text == "/del_par" && state.isAdmin)
+                {
+                    await bot.SendMessage(chatId, "send the tournament ID from which you want to remove a user");
+                    state.isWaitingForTIdToRemoveUser = true;
+                }
+                else if (text == "/support")
+                {
+                    await bot.SendMessage(chatId, "Ваше следуйщее сообщение отправиться в поддержку...");
+                    state.isWaitingForSupportMessage = true;
+                }
+                else if (text == "/del_T" && state.isAdmin)
+                {
+                    await bot.SendMessage(chatId, "send the tId you want to remove");
+                    state.isWaitingForTIdToRemove = true;
+                }
+                else if (text == "/par_list" && state.isAdmin)
+                {
+                    await bot.SendMessage(chatId, "send the tId you want to check");
+                    state.isWaitingForTIdToCheckList = true;
+                }
+                else if (text == "/playtime" && state.isAdmin)
+                {
+                    await bot.SendMessage(chatId, "send the tId of the tournament you want to start playing in");
+                    state.isWaitingForTIdToSendMessageToAllParticipants = true;
+                }
+                else if (text != null && state.isWaitingForIdToAdd)
                 {
                     state.isWaitingForIdToAdd = false;
                     state.IdToAdd = Convert.ToInt64(text);
                     state.isWaitingForTIDToAdd = true;
                     await bot.SendMessage(chatId, "send the tournament id");
+                }
+                else if (text != null && state.isWaitingForSupportMessage)
+                {
+                    state.isWaitingForSupportMessage = false;
+                    await bot.SendMessage(OwnerId, $"new support message from {update.Message.From}:\n{text}");
+                    await bot.SendMessage(chatId, $"Вы только что отрпавили '{text}' в поддержку.\n/support");
                 }
                 else if (text != null && state.isWaitingForTIDToAdd)
                 {
@@ -118,9 +171,20 @@ namespace SlayTheBot
                     {
                         if (state.TIdToAdd == i.tId)
                         {
-                            i._participants.Add(state.IdToAdd);
-                            Console.WriteLine($"new participant: {state.IdToAdd} in tournament {state.TIdToAdd}");
-                            break;
+                            if(i.availableSlots > 0)
+                            {
+                                i.availableSlots--;
+                                i._participants.Add(state.IdToAdd);
+                                await bot.SendMessage(chatId, $"new participant: {state.IdToAdd} in tournament {state.TIdToAdd}");
+                                Console.WriteLine($"new participant: {state.IdToAdd} in tournament {state.TIdToAdd}");
+                                SaveTournaments();
+                                break;
+                            }
+                            else
+                            {
+                                await bot.SendMessage(chatId, "no more available slots for this tournamnet!");
+                                break;
+                            }
                         }
                     }
                 }
@@ -130,6 +194,21 @@ namespace SlayTheBot
                     tName = text;
                     state.isWaitingForMaxParticipants = true;
                     await bot.SendMessage(chatId, "set the max participants count");
+                }
+                else if (text != null && state.isWaitingForTIdToCheckList)
+                {
+                    state.isWaitingForTIdToCheckList = false;
+                    foreach (var item in _tournaments)
+                    {
+                        if (item.tId == Convert.ToInt32(text))
+                        {
+                            foreach (var i in item._participants)
+                            {
+                                await bot.SendMessage(chatId, Convert.ToString(i));
+                            }
+                            break;
+                        }
+                    }
                 }
                 else if (text != null && state.isWaitingForMaxParticipants)
                 {
@@ -152,6 +231,78 @@ namespace SlayTheBot
                     Tournament tournament = new Tournament(tName, tMaxParticipants, tPrice, tId);
                     _tournaments.Add(tournament);
                     await bot.SendMessage(chatId, "Great! The tournament has been created.\n/create_T");
+                    SaveTournaments();
+                }
+                else if (text != null && state.isWaitingForTIdToRemove)
+                {
+                    state.isWaitingForTIDToAdd = false;
+                    foreach (var item in _tournaments)
+                    {
+                        if (item.tId == Convert.ToInt32(text))
+                        {
+                            _tournaments.Remove(item);
+                            await bot.SendMessage(chatId, $"great! tournament {text} has been removed");
+                            SaveTournaments();
+                            break;
+                        }
+                    }
+                }
+                else if (text != null && state.isWaitingForTIdToSendMessageToAllParticipants)
+                {
+                    state.isWaitingForTIdToSendMessageToAllParticipants = false;
+                    state.TIdToSendMessageToAllParticipants = Convert.ToInt32(text);
+                    state.isWaitingForMessageToSendToAllParticipants = true;
+                    await bot.SendMessage(chatId, "great! Now send the text you want to send to all the participants of the tournament you choose");
+                }
+                else if (text != null && state.isWaitingForMessageToSendToAllParticipants)
+                {
+                    foreach (var item in _tournaments)
+                    {
+                        if (item.tId == state.TIdToSendMessageToAllParticipants)
+                        {
+                            foreach (var item2 in item._participants)
+                            {
+                                await bot.SendMessage(item2, text);
+                            }
+                            break;
+                        }
+                    }
+                    await bot.SendMessage(chatId, "great! Messages were sent");
+                }
+                else if(text != null && state.isWaitingForTIdToRemoveUser)
+                {
+                    state.isWaitingForTIdToRemoveUser = false;
+                    state.TIdToRemoveUser = Convert.ToInt32(text);
+                    state.isWaitingForUserIdToRemove = true;
+                    await bot.SendMessage(chatId, "great! Now send the user id you want to remove");
+                }
+                else if(text != null && state.isWaitingForUserIdToRemove)
+                {
+                    state.isWaitingForUserIdToRemove = false;
+                    foreach(var item in _tournaments)
+                    {
+                        if(item.tId == state.TIdToRemoveUser)
+                        {
+                            foreach(var item2 in item._participants)
+                            {
+                                if(item2 == Convert.ToInt64(text))
+                                {
+                                    item.availableSlots++;
+                                    item._participants.Remove(item2);
+                                    await bot.SendMessage(chatId, $"great! User {item2} has been removed from tournament {state.TIdToRemoveUser}");
+                                    SaveTournaments();
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                else if(text != null && state.isWaitingForUserIdToConfirm)
+                {
+                    state.isWaitingForUserIdToConfirm = false;
+                    await bot.SendMessage(chatId, "great! Now add the user. /add_par");
+                    await bot.SendMessage(Convert.ToInt64(text), "Отлично! Админ @VERT1GO51 подтвердил ваше участие в турнире. Будьте доступны во время провведения игры.\nУдачи!\n/support");
                 }
 
             }
@@ -196,7 +347,7 @@ namespace SlayTheBot
                         {
                             foreach (var i in _tournaments)
                             {
-                                await bot.SendMessage(chatId, $"{i.name}.\nЧисло участников: {i.maxParticipants}\nЦена: {i.price} Бел. руб.\nID турнира: {i.tId}", replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Зарегистрироваться", $"reg {i.tId}")));
+                                await bot.SendMessage(chatId, $"{i.name}\nМаксимальное число участников: {i.maxParticipants}\nСвободных мест: {i.availableSlots}\nЦена: {i.price} Бел. руб.\nID турнира: {i.tId}", replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Зарегистрироваться", $"reg {i.tId}")));
                             }
                         }
                         else
@@ -218,6 +369,7 @@ namespace SlayTheBot
                 else if(messageData == "Перевод на карту")
                 {
                     int code = new Random().Next(1000, 9999);
+                    state.MessageCode = code;
                     await bot.SendMessage(chatId, $"Номер карты: 9112 3800 5670 1230 (Белорусбанк)\n\nВ комментарии к платежу укажите этот одноразовый код: {code}\n\nПосле оплаты отправьте скриншот успешного перевода денег в лс @VERT1GO51 и нажмите кнопку ниже. Дальше ожидайте подтверждения. Спасибо." 
                         , replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Оплатил и отправил скриншот!")));
                 }
@@ -228,17 +380,27 @@ namespace SlayTheBot
                     [("Телеграм Звёзды")]
                         });
                 }
+                else if(messageData == "Оплатил и отправил скриншот!")
+                {
+                    await bot.SendMessage(chatId, "Спасибо! Ожидайте подтверждения админа! \nЕсть вопросы - /support ;)");
+                    await bot.SendMessage(OwnerId, $"new user: {chatId} is maybe waiting for the confirmation for tournament\n message code: {state.MessageCode}", replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Confirm payment")));
+                }
+                else if(messageData == "Confirm payment")
+                {
+                    await bot.SendMessage(chatId, "send the id to confirm");
+                    state.isWaitingForUserIdToConfirm = true;
+                }
                 }
         }
     }
     class Tournament
     {
-        public string name;
-        public int participantCount = 0;
-        public int maxParticipants = 10;
-        public int price;
-        public int tId;
-        public List<long?> _participants = new List<long?>();
+        public string name { get; set; }
+        public int maxParticipants { get; set; }
+        public int availableSlots { get; set; }
+        public int price {  get; set; }
+        public int tId { get; set; }
+        public List<long?> _participants { get; set; } = new List<long?>();
 
         public Tournament(string name, int maxParticipants, int price, int tId)
         {
@@ -246,6 +408,7 @@ namespace SlayTheBot
             this.maxParticipants = maxParticipants;
             this.price = price;
             this.tId = tId;
+            availableSlots = maxParticipants;
         }
     }
 }
--- SlayTheBot/SlayTheBot/UserState.cs
+++ SlayTheBot/SlayTheBot/UserState.cs
@@ -11,9 +11,19 @@ class UserState
     public bool isWaitingForMaxParticipants { get; set; }
     public bool isWaitingForTournamentPrice { get; set; }
     public bool isWaitingForTournamentId { get; set; }
-    public int IdToReg {  get; set; }
     public bool isWaitingForTIDToAdd { get; set; }
     public long? IdToAdd { get; set; }
     public int TIdToAdd { get; set; }
     public bool isWaitingForIdToAdd { get; set; }
+    public bool isWaitingForSupportMessage { get; set; }
+    public bool isWaitingForTIdToRemove { get; set; }
+    public int MessageCode { get; set; }
+    public bool isWaitingForTIdToCheckList { get; set; }
+    public bool isWaitingForMessageToSendToAllParticipants { get; set; }
+    public bool isWaitingForTIdToSendMessageToAllParticipants { get; set; }
+    public int TIdToSendMessageToAllParticipants { get; set; }
+    public bool isWaitingForUserIdToRemove { get; set; }
+    public bool isWaitingForTIdToRemoveUser { get; set; }
+    public int TIdToRemoveUser { get; set; }
+    public bool isWaitingForUserIdToConfirm {  get; set; }
 }
